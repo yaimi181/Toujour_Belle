e6697442f5b53a32bad74596963ecb5f51902d5e
k4w411code <41393168+k4w411code@users.noreply.github.com>
2018-08-10 12:51:31 -0400

    Update Quiz.js

--- Quiz.js
+++ Quiz.js
@@ -5,13 +5,14 @@
 
         type= parseInt(document.querySelector('input[name = "type"]:checked').value);
 
-
         concern = parseInt(document.querySelector('input[name = "concern"]:checked').value);
+      
+        product = parseInt(document.querySelector('input[name = "product"]:checked').value);
 
-        total= type + concern;
+        total= type + concern + product;
 
     if(total==111) {
-    		document.getElementById("answer2").innerHTML = "We recommend <a href ="https://www.amazon.com/gp/product/B00IRAWK5S/ref=as_at/?imprToken=DX-MZW9u6rPoA32AJdnFZg&slotNum=2&ie=UTF8&tag=bustle5401-20&camp=1789&creative=9325&linkCode=w61&creativeASIN=B00IRAWK5S&linkId=6da8ce2a7653be434607b68e23cdd638">Ren Evercalm Gentle Cleansing Milk</a>";
+    		document.getElementById("answer3").innerHTML = "We recommend <a href ="https://www.amazon.com/gp/product/B00IRAWK5S/ref=as_at/?imprToken=DX-MZW9u6rPoA32AJdnFZg&slotNum=2&ie=UTF8&tag=bustle5401-20&camp=1789&creative=9325&linkCode=w61&creativeASIN=B00IRAWK5S&linkId=6da8ce2a7653be434607b68e23cdd638">Ren Evercalm Gentle Cleansing Milk</a>";
 
     	}
 
