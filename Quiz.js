2f879fb6438e4f688878fec09734e3bd7e7b270b
k4w411code <41393168+k4w411code@users.noreply.github.com>
2018-08-10 11:06:37 -0400

    Update Quiz.js

--- Quiz.js
+++ Quiz.js
@@ -11,8 +11,7 @@
         total= type + concern;
 
     if(total==183) {
-    		document.getElementById("answer2").innerHTML = "We recommend <a href ='https://www.amazon.com/gp/product/B00IRAWK5S/ref=as_at/?imprToken=DX-MZW9u6rPoA32AJdnFZg&slotNum=2&ie=UTF8&tag=bustle5401-20&camp=1789&creative=9325&linkCode=w61&creativeASIN=B00IRAWK5S&linkId=6da8ce2a7653be434607b68e23cdd638 
-'>Ren Evercalm Gentle Cleansing Milk</a>";
+    		document.getElementById("answer2").innerHTML = "We recommend <a href ="https://www.amazon.com/gp/product/B00IRAWK5S/ref=as_at/?imprToken=DX-MZW9u6rPoA32AJdnFZg&slotNum=2&ie=UTF8&tag=bustle5401-20&camp=1789&creative=9325&linkCode=w61&creativeASIN=B00IRAWK5S&linkId=6da8ce2a7653be434607b68e23cdd638">Ren Evercalm Gentle Cleansing Milk</a>";
 
     	}
 
